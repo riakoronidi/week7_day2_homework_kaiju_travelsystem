a2eed07aedaf1fd4b96a331bff42b83efa5db89e
Eleftheria Koronidi <ria@koronidi.com>
2018-03-06 20:01:42 +0000

    added test in BusTest to connect with ITravel

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,9 +1,18 @@
 public class Bus extends Earth{
 
-    private int number;
+    private String number;
 
-    public Bus(double travelCost, int number) {
+    public Bus(double travelCost, String number) {
         super(travelCost);
         this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String move(String movement) {
+        return "Bus " + movement + " goes through city centre";
     }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,4 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
 
     Bus bus;
+
+    @Before
+    public void setUp() throws Exception {
+        bus = new Bus(2.70, "B12");
+    }
+
+    @Test
+    public void testBusCanMove() {
+        assertEquals("Bus B12 goes through city centre", bus.move(bus.getNumber()));
+
+    }
 }
